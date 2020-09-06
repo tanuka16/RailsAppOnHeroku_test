eb2bb4fed16e02be78870e0a6b7281114d69e13a
Tanuka16 <tanuka.das12@gmail.com>
2020-09-06 14:41:09 -0400

    img

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,8 +32,8 @@ Rails.application.configure do
 
   config.serve_static_assets = true
 
-  # Compress JavaScripts and CSS
-  config.assets.compress = true
+  # # Compress JavaScripts and CSS
+  # config.assets.compress = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
