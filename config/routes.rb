1483ac1fa0c3c768cdac6f3b8581ed6fb0953c06
Tanuka16 <tanuka.das12@gmail.com>
2020-09-06 09:53:23 -0400

    set root routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :likes
   resources :users
 
+  root 'users#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
